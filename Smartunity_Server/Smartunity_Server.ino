1e8302fe5a83387f786e4cd0d2eb7f07121c5285
hms7709 <41275314+hms7709@users.noreply.github.com>
2019-12-16 15:33:39 +0900

    Update Smartunity_Server.ino

--- Smartunity_Server/Smartunity_Server.ino
+++ Smartunity_Server/Smartunity_Server.ino
@@ -1,37 +1,38 @@
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
-#include <DHT.h>
+#include <ESP8266WiFi.h>                // WiFI모듈 헤더파일
+#include <ESP8266HTTPClient.h>          // 클라이언트 헤더파일       
+#include <DHT.h>                        // DHT11 라이브러리 헤더파일
  
-#define PIN_DHT D4
+#define PIN_DHT D4                      // DHT11 Pin을 D4번으로 설정
  
-const char* ssid     = "Smartunity";
-const char* password = "unity1234";
-String host = "http://192.168.0.10";
-int brightness = A0; 
-const long interval = 5000;
-unsigned long previousMillis = 0;
+const char* ssid     = "Smartunity";    // "Wi-Fi ID"
+const char* password = "unity1234";     // "비밀번호"
+String host = "http://192.168.0.6";     // "Host 주소"
+int brightness = A0;                    // 조도센서 핀을 A0로 설정
+const long interval = 5000;             // 타이머 인터럽트를 위한 시간 간격 설정
+unsigned long previousMillis = 0;       
  
 WiFiServer server(80);
 WiFiClient client;
-HTTPClient http;
+HTTPClient http;                        // http 이름으로 HTTPClient 객체 생성
 DHT DHTsensor(PIN_DHT, DHT11);
  
 void setup() {
-  DHTsensor.begin();
-  pinMode(brightness, INPUT);
-  Serial.begin(115200);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  DHTsensor.begin();                    // DHT11 온습도 센서 초기화
+  pinMode(brightness, INPUT);           // 조도센서 핀 설정
+  Serial.begin(115200);                 // 통신 속도 115200bps
+  WiFi.mode(WIFI_STA);                  // WIFI_STA 모드 (다른 기기가 이 모듈을 통하여 인터넷 접속 금지)   
+  WiFi.begin(ssid, password);           // begin 메소드를 통한 Wi-Fi 연결
+  while (WiFi.status() != WL_CONNECTED) {  
     delay(500);
     Serial.print(".");
   }
+  // Wi-Fi 접속 상태 확인
   Serial.println("");
   Serial.print("Connecting to ");
   Serial.println(ssid);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-  server.begin();
+  server.begin();      // 접속된 Wi-Fi 아이디와 IP 주소를 시리얼 모니터에 출력
   Serial.println("Server started");
 }
  
@@ -44,22 +45,23 @@ void loop() {
     float Temperature = DHTsensor.readTemperature();
     int Cdsvalue = analogRead(brightness);
     String phpHost = host+"/insert.php?Temperature="+String(Temperature)+"&Humidity="+String(Humidity)+"&Cdsvalue="+String(Cdsvalue);
+    // 온습도센서값을 PHP 웹서버로 넘겨준다
     Serial.print("Connect to ");
     Serial.println(phpHost);
     
     http.begin(client, phpHost);
-    http.setTimeout(3000);
+    http.setTimeout(3000);     
     int httpCode = http.GET();
    
-    if(httpCode > 0) {
+    if(httpCode > 0) {  // 연결이 성공할 경우
       Serial.printf("GET code : %d\n\n", httpCode);
  
-      if(httpCode == HTTP_CODE_OK) {
+      if(httpCode == HTTP_CODE_OK) {   // 연결이 성공할 경우
         String payload = http.getString();
         Serial.println(payload);
       }
     } 
-    else {
+    else {   // 연결 실패할 경우
       Serial.printf("GET failed, error: %s\n", http.errorToString(httpCode).c_str());
     }
     http.end();
